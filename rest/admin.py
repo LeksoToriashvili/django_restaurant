9710aa9ddbbf5fcb478347677398280bce9cb05a
lexo <ltoriashvili@gmail.com>
2024-11-16 20:59:48 +0400

    added models

---
+++ rest/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from rest.models import Restaurant, Category, SubCategory, Dish, Ingredient
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'address', 'phone', 'image')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'restaurant')
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subcategory')
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name',)
---
+++ rest/models.py
@@ -0,0 +1,48 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Restaurant(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='restaurants')
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='categories')
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/', blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name='dishes')
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    dish = models.ManyToManyField(Dish, related_name='ingredients')
+
+    def __str__(self):
+        return self.name
